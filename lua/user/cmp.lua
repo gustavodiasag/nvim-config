68e4a3cf37a1288bd37e2df6d513cbdd0fd29af9
gustavodiasag <gustavodias.aguiar1@gmail.com>
2024-02-26 18:00:30 -0300

    feat(plugin): add nvim-cmp plugin

---
+++ lua/user/cmp.lua
@@ -0,0 +1,18 @@
+local M = {
+	-- Autocompletion
+	'hrsh7th/nvim-cmp',
+
+	dependencies = {
+		-- Snippet Engine & its associated nvim-cmp source
+		'L3MON4D3/LuaSnip',
+		'saadparwaiz1/cmp_luasnip',
+
+		-- Adds LSP completion capabilities
+		'hrsh7th/cmp-nvim-lsp',
+		'hrsh7th/cmp-path',
+
+		'rafamadriz/friendly-snippets',
+	}
+}
+
+return M
