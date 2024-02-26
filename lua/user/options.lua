605dc67740c5793078fb29064ccb9a155b98b008
gustavodiasag <gustavodias.aguiar1@gmail.com>
2024-02-26 14:02:05 -0300

    feat(vim): add config for yank highlighting

--- lua/user/options.lua
+++ lua/user/options.lua
@@ -22,3 +22,12 @@ vim.o.timeoutlen = 300
 vim.o.completeopt = 'menuone,noselect'
 
 vim.o.termguicolors = true
+
+local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
+vim.api.nvim_create_autocmd('TextYankPost', {
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+  group = highlight_group,
+  pattern = '*',
+})
