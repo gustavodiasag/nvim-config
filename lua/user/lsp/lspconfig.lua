1026e47f39f6593df1db4b5857dd59a0dbb40e56
gustavodiasag <gustavodias.aguiar1@gmail.com>
2024-02-28 19:17:28 -0300

    refac(lsp): formatting keybind

--- lua/user/lsp/lspconfig.lua
+++ lua/user/lsp/lspconfig.lua
@@ -2,11 +2,13 @@ local M = {
 	'neovim/nvim-lspconfig',
 
 	dependencies = {
+		'williamboman/mason.nvim',
+		'williamboman/mason-lspconfig.nvim',
 		'folke/neodev.nvim',
 	},
 }
 
-M.on_attach = function(_, bufnr)
+local on_attach = function(_, bufnr)
 	-- Helper function to easily define mapppings specific for
 	-- LSP-related items.
 	local nmap = function(key, fn, desc)
@@ -14,29 +16,32 @@ M.on_attach = function(_, bufnr)
 			desc = 'LSP: ' .. desc
 		end
 
-		vim.keyma.set('n', key, fn, { buffer =  bufnr, desc = desc })
+		vim.keymap.set('n', key, fn, { buffer = bufnr, desc = desc })
 	end
 
 	local builtin = require('telescope.builtin')
 
-  nmap('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
-  nmap('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
-  nmap('gd', builtin.lsp_definitions, '[G]oto [D]efinition')
-  nmap('gr', builtin.lsp_references, '[G]oto [R]eferences')
-  nmap('gI', builtin.lsp_implementations, '[G]oto [I]mplementation')
-  nmap('<leader>D', builtin.lsp_type_definitions, 'Type [D]efinition')
-  nmap('<leader>ds', builtin.lsp_document_symbols, '[D]ocument [S]ymbols')
-  nmap('<leader>ws', builtin.lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
-  nmap('K', vim.lsp.buf.hover, 'Hover Documentation')
-  nmap('<C-k>', vim.lsp.buf.signature_help, 'Signature Documentation')
-  nmap('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
-  nmap('<leader>wa', vim.lsp.buf.add_workspace_folder, '[W]orkspace [A]dd Folder')
-  nmap('<leader>wr', vim.lsp.buf.remove_workspace_folder, '[W]orkspace [R]emove Folder')
-  nmap('<leader>wl', function()
-    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-  end, '[W]orkspace [L]ist Folders')
+	nmap('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
+	nmap('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
+	nmap('gd', builtin.lsp_definitions, '[G]oto [D]efinition')
+	nmap('gr', builtin.lsp_references, '[G]oto [R]eferences')
+	nmap('gI', builtin.lsp_implementations, '[G]oto [I]mplementation')
+	nmap('<leader>D', builtin.lsp_type_definitions, 'Type [D]efinition')
+	nmap('<leader>ds', builtin.lsp_document_symbols, '[D]ocument [S]ymbols')
+	nmap('<leader>ws', builtin.lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
+	nmap('K', vim.lsp.buf.hover, 'Hover Documentation')
+	nmap('<C-k>', vim.lsp.buf.signature_help, 'Signature Documentation')
+	nmap('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
+	nmap('<leader>wa', vim.lsp.buf.add_workspace_folder, '[W]orkspace [A]dd Folder')
+	nmap('<leader>wr', vim.lsp.buf.remove_workspace_folder, '[W]orkspace [R]emove Folder')
+	nmap('<leader>wl', function()
+		print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+	end, '[W]orkspace [L]ist Folders')
 
-	nmap('<leader>F', vim.lsp.buf.format(), '[F]ormat current buffer with LSP')	
+	-- Formatting
+	vim.keymap.set('n', '<leader>f', function()
+		vim.lsp.buf.format { async = true }
+	end, {})
 end
 
 function M.capabilities()
@@ -57,6 +62,19 @@ function M.config()
 	}
 
 	require('neodev').setup()
+
+	local mason_lspconfig = require 'mason-lspconfig'
+
+	mason_lspconfig.setup_handlers {
+		function(server_name)
+			require('lspconfig')[server_name].setup {
+				capabilities = M.capabilities(),
+				on_attach = on_attach,
+				settings = servers[server_name],
+				filetypes = (servers[server_name] or {}).filetypes,
+			}
+		end,
+	}
 end
 
 return M
