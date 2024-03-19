e430c0397356193ee16455592113fc6655e8b965
Gustavo Dias <gustavodias.aguiar1@gmail.com>
2024-03-19 17:12:52 -0300

    style: separate options by category

--- lua/user/options.lua
+++ lua/user/options.lua
@@ -1,32 +1,29 @@
 local opt = vim.o
 
-opt.hlsearch = false
-
-vim.wo.number = true
-vim.wo.relativenumber = true
-
-opt.mouse = 'a'
-
-opt.cursorline = true
-
-opt.clipboard = 'unnamedplus'
-
-opt.breakindent = true
-
-opt.undofile = true
-
-opt.ignorecase = true
-opt.smartcase = true
-
-vim.wo.signcolumn = 'yes'
-
-opt.updatetime = 250
-opt.timeoutlen = 300
-
+-- Completion
 opt.completeopt = 'menuone,noselect'
 
+-- Editor
+opt.number = true
+opt.relativenumber = true
+opt.breakindent = true
+opt.cursorline = true
+opt.clipboard = 'unnamedplus'
+opt.signcolumn = 'yes'
+opt.undofile = true
+
+-- Search
+opt.ignorecase = true
+opt.smartcase = true
+opt.hlsearch = false
+
+-- System
+opt.updatetime = 250
+opt.timeoutlen = 300
+opt.mouse = 'a'
 opt.termguicolors = true
 
+-- Visual highlighting for the 'yank' command
 local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
 vim.api.nvim_create_autocmd('TextYankPost', {
 	callback = function()
