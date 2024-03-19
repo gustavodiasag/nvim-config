a8534db1a6fe347d95736f53aae26bef17d7d4db
Gustavo Dias <gustavodias.aguiar1@gmail.com>
2024-03-19 14:39:04 -0300

    feat(gitsigns): add keymap configs

--- init.lua
+++ init.lua
@@ -1,21 +1,22 @@
 -- Base mappings and settings
-require("user.launch")
-require("user.options")
-require("user.keymaps")
+require('user.launch')
+require('user.options')
+require('user.keymaps')
 
 -- General purpose plugins
-spec("user.whichkey")
-spec("user.telescope")
-spec("user.colorscheme")
-spec("user.cmp")
+spec('user.whichkey')
+spec('user.telescope')
+spec('user.colorscheme')
+spec('user.cmp')
 
 -- LSṔ-related plugins
-spec("user.lsp.mason")
-spec("user.lsp.mason-lspconfig")
-spec("user.lsp.lspconfig")
+spec('user.lsp.mason')
+spec('user.lsp.mason-lspconfig')
+spec('user.lsp.lspconfig')
 
 -- Git-related plugins
-spec("user.git.fugitive")
-spec("user.git.rhubarb")
+spec('user.git.fugitive')
+spec('user.git.rhubarb')
+spec('user.git.gitsigns')
 
-require("user.lazy")
+require('user.lazy')
