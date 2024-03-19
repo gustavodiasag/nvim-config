96bc3ccb67015ff7f18db71a62d4ccb52e30656a
Gustavo Dias <gustavodias.aguiar1@gmail.com>
2024-03-19 15:30:56 -0300

    style: formatting

--- lua/user/treesitter.lua
+++ lua/user/treesitter.lua
@@ -8,7 +8,6 @@ local M = {
 
 M.config = function()
 	require('nvim-treesitter.configs').setup {
-		-- Add languages to be installed here that you want installed for treesitter
 		ensure_installed = {
 			'c',
 			'cpp',
@@ -22,13 +21,13 @@ M.config = function()
 			'bash',
 		},
 
-		-- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
+		-- Autoinstall languages that are not installed
 		auto_install = false,
-		-- Install languages synchronously (only applied to `ensure_installed`)
+		-- Install languages synchronously
 		sync_install = false,
 		-- List of parsers to ignore installing
 		ignore_install = {},
-		-- You can specify additional Treesitter modules here: -- For example: -- playground = {--enable = true,-- },
+
 		modules = {},
 		highlight = { enable = true },
 		indent = { enable = true },
@@ -44,9 +43,8 @@ M.config = function()
 		textobjects = {
 			select = {
 				enable = true,
-				lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
+				lookahead = true, -- Automatically jump forward to textobj
 				keymaps = {
-					-- You can use the capture groups defined in textobjects.scm
 					['aa'] = '@parameter.outer',
 					['ia'] = '@parameter.inner',
 					['af'] = '@function.outer',
